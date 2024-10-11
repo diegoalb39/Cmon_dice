5d71db3217a0e8e4a951a9cb15df597b3f6ab91f
diegoalb39 <diegoalbornoz39@gmail.com>
2024-10-11 19:43:47 -0300

    Add queue operations

--- listaDinamica/lista.h
+++ listaDinamica/lista.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NO_MEMORY 0
+#define EMPTY 0
+#define ALL_OK 1
+
+#define MINIMO(x,y) ((x)<(y) ? (x) : (y))
+
 typedef struct s_nodo
 {
     void *dato;
@@ -13,7 +19,7 @@ typedef struct s_nodo
 
 typedef struct
 {
-    t_nodo *ini;
+    t_nodo *pri;
     t_nodo *ult;
 }t_cola;
 
@@ -24,6 +30,14 @@ typedef struct
 }t_lista_it; //lista iteradora
 
 typedef t_nodo* lista;
+
+void crearCola(t_cola *pc);
+void vaciarCola(t_cola *pc);
+int colaVacia(const t_cola *pc);
+int colaLlena(const t_cola *pc, unsigned tam);
+int ponerEnCola(t_cola *pc, const void *info, unsigned tam);
+int verPrimeroCola(const t_cola *pc, void *dato, unsigned tam);
+int sacarDeCola(t_cola *pc, void *buffer, unsigned tam);
 
 
 #endif // LISTA_H_INCLUDED
